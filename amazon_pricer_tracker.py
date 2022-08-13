314487ebbf1ab74c0f6bd1287248df061667c3bc
mixed_nuts_nuts <french0719@gmail.com>
2022-08-13 12:32:18 +0700

    modify url

--- amazon_pricer_tracker.py
+++ amazon_pricer_tracker.py
@@ -27,7 +27,7 @@ def send_line_notification():
         ACCESSTOKEN = TOKEN
         line_notify_api = "https://notify-api.line.me/api/notify"
         headers = {"authorization":f"Bearer {ACCESSTOKEN}"}
-        data = {"message":"NOW SALL!! https://www.amazon.co.jp/Python%E3%81%A7%E3%81%AF%E3%81%98%E3%82%81%E3%82%8B%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92-%E2%80%95scikit-learn%E3%81%A7%E5%AD%A6%E3%81%B6%E7%89%B9%E5%BE%B4%E9%87%8F%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%A8%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E3%81%AE%E5%9F%BA%E7%A4%8E-Andreas-C-Muller/dp/4873117984/ref=sr_1_8?keywords=python%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92&qid=1660149428&sprefix=Python%E6%A9%9F%E6%A2%B0%2Caps%2C383&sr=8-8" }
+        data = {"message":"Now! {}".format(amazon_url)}
         requests.post(line_notify_api, headers=headers,data=data)
 
 # # 自動呼び出し
